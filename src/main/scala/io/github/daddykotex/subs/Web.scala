aab1fee2c1c1732a2508f808b1ed34e6711d568e
David Francoeur <dfrancoeur04@gmail.com>
2017-08-22 09:15:16 -0400

    feat(html): very simple sign up page with bootstrap

---
+++ src/main/scala/io/github/daddykotex/subs/Web.scala
@@ -0,0 +1,34 @@
+package io.github.daddykotex.subs
+
+import cats.data._
+import cats.implicits._
+
+import fs2.interop.cats._
+
+import org.http4s._
+import org.http4s.twirl._
+import org.http4s.server._
+import org.http4s.dsl._
+import org.log4s.getLogger
+
+object Web {
+  private[this] val logger = getLogger
+
+  val app = HttpService {
+    // Note: No `Root` this is due to a limitation in the language, it sucks, but that's life
+    case request @ GET -> "static" /: path =>
+      StaticFile.fromResource(path.toString, Some(request)).getOrElseF(NotFound())
+
+    case request @ POST -> Root / "signup" =>
+      request.decode[UrlForm] { data =>
+        data.getFirst("email") map { email =>
+          SeeOther(request.uri.copy(path = "/signup"))
+        } getOrElse {
+          BadRequest(html.signup("Sign up"))
+        }
+      }
+
+    case GET -> Root / "signup" =>
+      Ok(html.signup("Sign up"))
+  }
+}
